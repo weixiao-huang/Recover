f104451b2f121af23c6e2c47e4101b275c346848
Huang Smile <18811351753@163.com>
2016-05-26 14:16:14 +0800

    Initial

---
+++ src/com/main/main.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import com.util.NPuzzle;
+
+/**
+ * Created by Mike Huang on 2016/5/26.
+ */
+public class main {
+    public static void main(String[] args) {
+        NPuzzle nPuzzle = new NPuzzle();
+        System.out.println(nPuzzle);
+    }
+}
